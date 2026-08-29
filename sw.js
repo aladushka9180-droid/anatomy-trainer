530382f3fdaec14bb7551e21522ce4d18dc9fc8c
aladushka9180-droid <aladushka9180@gmail.com>
2026-08-29 22:38:47 +0400

    Уточнить выбор формата обучения

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='anatomy-v39';
+const CACHE='anatomy-v40';
 const ASSETS=['./','./index.html','./minimal-redesign.css?v=38','./professional-learning.css?v=39','./massage-data.js','./practice-cases.js','./learning-paths.js','./learning-sources.js','./reference-data.js','./massage-techniques.js?v=38','./practice-curriculum.js?v=38','./professional-learning.js?v=39','./ai-assistant.js?v=38','./manifest.webmanifest','./icon-192.png','./icon-512.png','./anatomy-upper-limb.png','./anatomy-leg.png','./anatomy-calf.png','./anatomy-gluteal.png','./anatomy-adductors.png','./anatomy-back.png','./anatomy-neck.png','./bones-skull.svg','./bones-upper-limb.svg','./bones-spine.png','./bones-vertebrae.png','./bones-lower-limb.svg','./practice-compression.png','./practice-percussion.png','./practice-passive-stretch.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('anatomy-')&&k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
