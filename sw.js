571758c72eb8ca4e03f9936dc86e4287619d60f8
aladushka9180-droid <aladushka9180@gmail.com>
2026-08-29 12:43:50 +0400

    Добавить выбор голоса озвучки
    Добавлен список доступных русских голосов устройства, предварительное прослушивание и сохранение выбранного голоса. Пользователь может отдельно выбрать голос и скорость речи.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='anatomy-v10';
+const CACHE='anatomy-v11';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./anatomy-upper-limb.png','./anatomy-leg.png','./anatomy-gluteal.png','./anatomy-adductors.png','./anatomy-back.png','./anatomy-neck.png','./bones-skull.svg','./bones-upper-limb.svg','./bones-spine.png','./bones-vertebrae.png','./bones-lower-limb.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
