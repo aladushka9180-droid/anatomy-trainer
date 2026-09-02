c8ea2c8b9a6ff09f418c9d141079f9027f30e185
aladushka9180-droid <aladushka9180-droid@users.noreply.github.com>
2026-09-02 20:42:03 +0400

    Упростить карточку групповых событий

--- minuta-online-booking/waitlist.js
+++ minuta-online-booking/waitlist.js
@@ -44,4 +44,4 @@ $('#cancelWaitlist').addEventListener('click', async () => {
 });
 
 loadRequest();
-if ('serviceWorker' in navigator) window.addEventListener('load', () => navigator.serviceWorker.register('./sw.js?v=172'));
+if ('serviceWorker' in navigator) window.addEventListener('load', () => navigator.serviceWorker.register('./sw.js?v=173'));
