e3ee82e966a8226d7a556fefd3b74e5b56683b95
aladushka9180-droid <aladushka9180-droid@users.noreply.github.com>
2026-09-04 13:39:00 +0400

    Bump site cache to v298

--- minuta-online-booking/site-update.js
+++ minuta-online-booking/site-update.js
@@ -3,7 +3,7 @@
 
   if (!('serviceWorker' in navigator)) return;
 
-  const workerUrl = './sw.js?v=296';
+  const workerUrl = './sw.js?v=298';
   let registration = null;
   let currentController = navigator.serviceWorker.controller;
   let lastCheck = 0;
