30ead0d22b302dfc7da90ea89e44cd4189797abb
aladushka9180-droid <aladushka9180-droid@users.noreply.github.com>
2026-09-04 17:22:30 +0400

    Release past visit creation as v321

--- minuta-online-booking/site-update.js
+++ minuta-online-booking/site-update.js
@@ -3,7 +3,7 @@
 
   if (!('serviceWorker' in navigator)) return;
 
-const workerUrl = './sw.js?v=320';
+const workerUrl = './sw.js?v=321';
   let registration = null;
   let currentController = navigator.serviceWorker.controller;
   let lastCheck = 0;
