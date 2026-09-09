043a5687d243fd6259cb9025bd0ebf1ff1006a3a
aladushka9180-droid <aladushka9180-droid@users.noreply.github.com>
2026-09-09 15:34:10 +0400

    chore: bump booking widgets release to v658

--- minuta-online-booking/site-update.js
+++ minuta-online-booking/site-update.js
@@ -4,7 +4,7 @@
   if (!('serviceWorker' in navigator)) return;
 
   const scriptUrl = document.currentScript?.src || location.href;
-  const workerUrl = new URL('./sw.js?v=657', scriptUrl).href;
+  const workerUrl = new URL('./sw.js?v=658', scriptUrl).href;
   const CHECK_INTERVAL_MS = 15 * 60 * 1000;
   let registration = null;
   let currentController = navigator.serviceWorker.controller;
